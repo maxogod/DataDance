3c7943fa1bc5ac1cfaa919e7c7b3821c250ac874
maxogod <maxi18wars@gmail.com>
2024-04-23 22:41:46 -0300

    adding remove command

--- commands/remove_command.py
+++ commands/remove_command.py
@@ -1,6 +1,8 @@
 from commands.command import Command
 from state.global_state import GlobalState
 from errors import TooManyArguments, TooFewArguments, BadCommandUse
+import utils
+import os
 
 
 class RemoveCommand(Command):
@@ -8,11 +10,22 @@ class RemoveCommand(Command):
     removes database
     """
 
+    database_to_remove = ""
+
     def __init__(self, args: list[str]):
         if len(args) < 1:
             raise TooFewArguments("rm <database_path>")
         if len(args) > 1:
             raise TooManyArguments("rm <database_path>")
 
+        if GlobalState.in_db_mode():
+            raise BadCommandUse(
+                "You must be in normal mode to remove a database")
+
+        if not utils.is_db_file(args[0]):
+            raise BadCommandUse("File is not a database")
+
+        self.database_to_remove = args[0]
+
     def execute(self):
-        pass
+        os.remove(self.database_to_remove)
